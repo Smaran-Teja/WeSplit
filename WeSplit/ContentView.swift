8b838b9eac1d18d8431a7c5d6ae9233566482dd5
Smaran-Teja-P <penikelapati.sm@northeastern.edu>
2024-05-14 19:57:33 -0400

    Added num people scroller, tip selector

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,19 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var checkAmount = 0.0
-    @State private var numberOfPeople = 2
+    @State private var numberOfPeople = 0
     @State private var tipPercentage = 20
     
+    private let tipPercentages = [0, 5, 10, 15, 20]
+    
     var body: some View {
-        Form {
-            Section {
-                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                    .keyboardType(.decimalPad)
-            }
+        NavigationStack{
+            Form {
+                Section ("Total amount"){
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                }
+                
+                Section ("Number of people") {
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<20) {
+                            Text("\($0) people")
+                        }
+                    }
+                    .pickerStyle(.wheel)
+                }
+                
+                Section ("Tip percentage") {
+                    Picker("Number of people", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0,format: .percent)
 
-            Section {
-                Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-            }
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section("You need to pay...") {
+                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }.navigationTitle("WeSplit")
         }
     }
 }
