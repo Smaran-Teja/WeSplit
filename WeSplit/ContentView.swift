8d62fe86c98d75aa4e5ccdb6ccaacf2c4d83c371
Smaran-Teja-P <penikelapati.sm@northeastern.edu>
2024-05-14 14:17:12 -0400

    Added navbar

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,17 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State
-    private var tapCount = 1438791
-
     var body: some View {
-        Button("Tap Count: \(tapCount)") {
-            if tapCount.isMultiple(of: 2) {
-                tapCount /= 2
+        NavigationStack {
+            Form {
+                Section {
+                    Text("Hello, world!")
+                }
             }
-            else {
-                tapCount = 3 * tapCount + 1
-            }
+            .navigationTitle("SwiftUI")
         }
     }
 }
